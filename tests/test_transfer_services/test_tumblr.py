a46256c082b7521c4699ab8050450f821c93da49
Arthur Borem <arthurborem@uchicago.edu>
2025-08-12 17:09:05 -0500

    Update Tumblr's scope to basic instead of base (#44)
    Closes #43

--- tests/test_transfer_services/test_tumblr.py
+++ tests/test_transfer_services/test_tumblr.py
@@ -6,7 +6,7 @@ sample_scope = {'fake', 'scope'}
 
 
 @pytest.mark.parametrize(
-    ['verticals', 'expected_scope'], [([], {'base'}), ([Vertical.FeedPost, {'base'}])]
+    ['verticals', 'expected_scope'], [([], {'basic'}), ([Vertical.FeedPost, {'basic'}])]
 )
 def test_scope_for_vertical(mock_tumblr_transfer_service, verticals, expected_scope):
     assert mock_tumblr_transfer_service.scope_for_verticals(verticals) == expected_scope
