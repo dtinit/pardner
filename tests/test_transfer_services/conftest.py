f470b40b4539f9f7ac80a8ee60bdd23925a61e8f
Arthur Borem <arthurborem@uchicago.edu>
2025-08-13 16:18:32 -0500

    Implements fetching tumblr posts from feed

--- tests/test_transfer_services/conftest.py
+++ tests/test_transfer_services/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from requests_oauthlib import OAuth2Session
 
 from pardner.services.strava import StravaTransferService
 from pardner.services.tumblr import TumblrTransferService
@@ -6,12 +7,15 @@ from pardner.verticals.base import Vertical
 
 
 @pytest.fixture
-def mock_oAuth2Session(mocker):
-    mock_oauth2session_request = mocker.patch('requests_oauthlib.OAuth2Session.request')
-    mock_client_parse_request_body_response = mocker.patch(
+def mock_oauth2_session_request(mocker):
+    return mocker.patch('requests_oauthlib.OAuth2Session.request')
+
+
+@pytest.fixture
+def mock_oauth2_session_response(mocker):
+    return mocker.patch(
         'oauthlib.oauth2.rfc6749.clients.WebApplicationClient.parse_request_body_response'
     )
-    return [mock_oauth2session_request, mock_client_parse_request_body_response]
 
 
 @pytest.fixture
@@ -32,3 +36,9 @@ def mock_strava_transfer_service(verticals=[Vertical.FeedPost]):
     return StravaTransferService(
         'fake_client_id', 'fake_client_secret', 'https://redirect_uri', None, verticals
     )
+
+
+def mock_oauth2_session_get(mocker, response_object):
+    oauth2_session_get = mocker.patch.object(OAuth2Session, 'get', autospec=True)
+    oauth2_session_get.return_value = response_object
+    return oauth2_session_get
--- tests/test_transfer_services/test_transfer_services_common.py
+++ tests/test_transfer_services/test_transfer_services_common.py
@@ -5,10 +5,17 @@ import pytest
     'mock_transfer_service_name',
     ['mock_tumblr_transfer_service', 'mock_strava_transfer_service'],
 )
-def test_fetch_token(mock_oAuth2Session, request, mock_transfer_service_name):
-    [mock_request, _] = mock_oAuth2Session
+def test_fetch_token(
+    mock_oauth2_session_request,
+    mock_oauth2_session_response,
+    request,
+    mock_transfer_service_name,
+):
     mock_transfer_service = request.getfixturevalue(mock_transfer_service_name)
     mock_transfer_service.fetch_token(code='123code123')
-    mock_request.assert_called_once()
-    assert 'client_id' in mock_request.call_args.kwargs['data']
-    assert mock_request.call_args.kwargs['data']['client_id'] == 'fake_client_id'
+    mock_oauth2_session_request.assert_called_once()
+    assert 'client_id' in mock_oauth2_session_request.call_args.kwargs['data']
+    assert (
+        mock_oauth2_session_request.call_args.kwargs['data']['client_id']
+        == 'fake_client_id'
+    )
