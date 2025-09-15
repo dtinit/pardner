1a20c95f02f4c992512f7182ccd7693bdab56799
Arthur Borem <arthurborem@uchicago.edu>
2025-09-15 17:00:35 -0500

    Adds description for media type

--- src/pardner/verticals/sub_verticals/associated_media.py
+++ src/pardner/verticals/sub_verticals/associated_media.py
@@ -1,6 +1,6 @@
 from typing import Literal
 
-from pydantic import AnyHttpUrl
+from pydantic import AnyHttpUrl, Field
 
 from pardner.verticals.sub_verticals.base import BaseSubVertical
 
@@ -8,5 +8,9 @@ from pardner.verticals.sub_verticals.base import BaseSubVertical
 class AssociatedMediaSubVertical(BaseSubVertical):
     """Holds the URL for the media attached to a parent vertical."""
 
-    media_type: Literal['audio', 'image', 'video'] | None = None
+    media_type: Literal['audio', 'image', 'video'] | None = Field(
+        description='If media type is unknown or not one of the types defined in this '
+        'field, value is None.',
+        default=None,
+    )
     url: AnyHttpUrl | None = None
