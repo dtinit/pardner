e1cb2f7bf96c58b6417a937137f2c6420d97fe52
Arthur Borem <arthurborem@uchicago.edu>
2025-09-29 12:11:08 -0500

    Tumblr method for fetching primary blog ID (used as user ID) (#79)
    Closes #77 
    
    New Tumblr method fetches the primary blog id. This is useful for
    identifying the Tumblr user in their posts, comments, likes, etc. when
    fetching other verticals

--- src/pardner/services/tumblr.py
+++ src/pardner/services/tumblr.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Iterable, Optional, override
 
 from pardner.exceptions import UnsupportedRequestException
@@ -12,6 +13,7 @@ class TumblrTransferService(BaseTransferService):
     See API documentation: https://www.tumblr.com/docs/en/api/v2
     """
 
+    primary_blog_id: str | None = None
     _authorization_url = 'https://www.tumblr.com/oauth2/authorize'
     _base_url = 'https://api.tumblr.com/v2/'
     _token_url = 'https://api.tumblr.com/v2/oauth2/token'
@@ -23,7 +25,20 @@ class TumblrTransferService(BaseTransferService):
         redirect_uri: str,
         state: Optional[str] = None,
         verticals: set[Vertical] = set(),
+        primary_blog_id: str | None = None,
     ) -> None:
+        """
+        Creates an instance of ``TumblrTransferService``.
+
+        :param client_id: Client identifier given by the OAuth provider upon registration.
+        :param client_secret: The ``client_secret`` paired to the ``client_id``.
+        :param redirect_uri: The registered callback URI.
+        :param state: State string used to prevent CSRF and identify flow.
+        :param verticals: The :class:`Vertical`s for which the transfer service has
+        appropriate scope to fetch.
+        :param primary_blog_id: Optionally, the primary blog ID of the data owner (the
+        user being authorized).
+        """
         super().__init__(
             service_name='Tumblr',
             client_id=client_id,
@@ -33,6 +48,7 @@ class TumblrTransferService(BaseTransferService):
             supported_verticals={SocialPostingVertical},
             verticals=verticals,
         )
+        self.primary_blog_id = primary_blog_id
 
     @override
     def scope_for_verticals(self, verticals: Iterable[Vertical]) -> set[str]:
@@ -47,6 +63,40 @@ class TumblrTransferService(BaseTransferService):
         include_client_id: bool = True,
     ) -> dict[str, Any]:
         return super().fetch_token(code, authorization_response, include_client_id)
+
+    def fetch_primary_blog_id(self) -> str:
+        """
+        Fetches the primary blog ID from the data owner, which will be used as the
+        ``data_owner_id`` in the vertical model objects. If the ``primary_blog_id``
+        attribute on this class is already set, the method does not make a new request.
+
+        Note: "PrimaryBlogId" is not a vertical. This is used purely as a unique
+        identifier for the user, since Tumblr doesn't provide one by default.
+
+        :returns: the primary blog id.
+
+        :raises: :class:`ValueError`: if the primary blog ID could not be extracted from
+        the response.
+        """
+        if self.primary_blog_id:
+            return self.primary_blog_id
+        user_info = self._get_resource_from_path('user/info').json().get('response', {})
+        for blog_info in user_info.get('user', {}).get('blogs', []):
+            if (
+                isinstance(blog_info, dict)
+                and blog_info.get('primary')
+                and 'uuid' in blog_info
+                and isinstance(blog_info['uuid'], str)
+            ):
+                self.primary_blog_id = blog_info['uuid']
+                return blog_info['uuid']
+
+        raise ValueError(
+            'Failed to fetch primary blog id. Either manually set the _primary_blog_id '
+            'attribute or verify all the client credentials '
+            'and permissions are correct. Response from Tumblr: '
+            f'{json.dumps(user_info, indent=2)}'
+        )
 
     def fetch_social_posting_vertical(
         self,
