173a8d755061c5ab0ffe8495211a3dfb60886829
Arthur Borem <arthurborem@uchicago.edu>
2025-09-29 09:32:06 -0500

    Tumblr method for fetching primary blog ID (used as user ID)

--- tests/test_transfer_services/test_tumblr.py
+++ tests/test_transfer_services/test_tumblr.py
@@ -40,3 +40,35 @@ def test_fetch_social_posting_vertical(mocker, tumblr_transfer_service):
         oauth2_session_get.call_args.args[1]
         == 'https://api.tumblr.com/v2/user/dashboard'
     )
+
+
+def test_fetch_primary_blog_id_already_set(tumblr_transfer_service):
+    tumblr_transfer_service.primary_blog_id = 'existing-blog-id'
+    assert tumblr_transfer_service.fetch_primary_blog_id() == 'existing-blog-id'
+
+
+def test_fetch_primary_blog_id_success(mocker, tumblr_transfer_service):
+    response_object = mocker.MagicMock()
+    response_object.json.return_value = {
+        'response': {
+            'user': {
+                'blogs': [
+                    {'primary': False, 'uuid': 'secondary-blog-id'},
+                    {'primary': True, 'uuid': 'primary-blog-id', 'name': 'my-blog'},
+                    {'primary': False, 'uuid': 'another-secondary-id'},
+                ]
+            }
+        }
+    }
+    oauth2_session_get = mock_oauth2_session_get(mocker, response_object)
+
+    assert tumblr_transfer_service.fetch_primary_blog_id() == 'primary-blog-id'
+    assert tumblr_transfer_service.primary_blog_id == 'primary-blog-id'
+    assert oauth2_session_get.call_args.args[1] == 'https://api.tumblr.com/v2/user/info'
+
+
+def test_fetch_primary_blog_id_raises_exception(
+    tumblr_transfer_service, mock_oauth2_session_get_bad_response
+):
+    with pytest.raises(HTTPError):
+        tumblr_transfer_service.fetch_primary_blog_id()
